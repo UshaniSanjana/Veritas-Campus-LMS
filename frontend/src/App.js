4bfbb90a95f102d26f1d5f3371af8bcf4dea2d7c
Ushani Sanjana <ushsanjana@gmail.com>
2025-04-24 14:16:21 +0530

    Import bootstrap into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Button from './components/Button';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
