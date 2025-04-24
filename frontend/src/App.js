2cfffa0b7a04da83ece09c0a3d314990986bcc84
Ushani Sanjana <ushsanjana@gmail.com>
2025-04-24 14:32:10 +0530

    Added paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
 import Button from './components/Button';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Home from "./pages/static/Home";
+import Navbar from "./components/Navbar";
+import WhoWeAre from "./pages/static/WhoWeAre";
+import Programmes from "./pages/static/Programmes";
+import News from "./pages/static/News";
+import ContactUs from "./pages/static/ContactUs";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -9,7 +16,15 @@ function App() {
   return (
     <div>
       <Navbar/>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/whoweare" element={<WhoWeAre />} />
+        <Route path="/programmes" element={<Programmes />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/contactus" element={<ContactUs />} />
+      </Routes>
       <Button/>
+    
       <Footer />
     </div>
   );
