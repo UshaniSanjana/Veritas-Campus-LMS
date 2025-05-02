adaeaa319b0912f2a5ccf34b856c700746909a0c
Anji-fdo <131850714+Anji-fdo@users.noreply.github.com>
2025-05-02 11:57:06 +0530

    Update Addquiz page

--- frontend/src/pages/instructor/AddQuiz.jsx
+++ frontend/src/pages/instructor/AddQuiz.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import '../../App.css';
 
@@ -19,6 +19,8 @@ const AddQuiz = () => {
       },
     ],
   });
+
+  const navigate = useNavigate(); 
 
   const [isPopupVisible, setIsPopupVisible] = useState(false); 
 
@@ -66,7 +68,7 @@ const AddQuiz = () => {
   
     try {
       const res = await axios.post("http://localhost:5000/api/quizzes", quizData);
-      alert("Quiz saved successfully!");
+      
       console.log("Response:", res.data);
   
       // Reset form after successful submission
@@ -85,6 +87,8 @@ const AddQuiz = () => {
           },
         ],
       });
+
+      navigate("/quizlist");
   
     } catch (err) {
       console.error("Error saving quiz:", err);
