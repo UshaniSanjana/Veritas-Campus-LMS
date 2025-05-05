b4e51da4ac002fc4475157bd89290015bd13f316
Ushani-Sanjana <ush.sanjana@gmail.com>
2025-05-05 13:06:17 +0530

    added improved student model

--- backend/models/student.model.js
+++ backend/models/student.model.js
@@ -1,39 +1,43 @@
 const mongoose = require("mongoose");
 
-const studentSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    degree: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
   },
-  age: {
-    type: Number,
-    required: true,
-  },
-  //   address: {
-  //     type: String,
-  //     required: true,
-  //   },
-  //   gender: {
-  //     type: String,
-  //     enum: ["female", "male"],
-  //   },
-  //   email: {
-  //     type: String,
-  //     required: true,
-  //   },
-  //   mobile: {
-  //     type: String,
-  //     required: true,
-  //   },
-  //   degree: {
-  //     type: String,
-  //     required: true,
-  //   },
-  image: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Student = mongoose.model("Student", studentSchema);
 
