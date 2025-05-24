9c4b123082840e8e90d159eae24f953b9238eb07
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-05-24 13:50:30 +0530

    Programme Sections, Add Update Delete Announcments

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const announcementRoutes = require('./routes/announcementRoute');
 
-//dotenv
 require('dotenv').config();
-
 const app = express();
-const {mongoose} = require('mongoose');
 
-//database connection
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use('/', announcementRoutes);
+
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('MongoDB Connected'))
-    .catch((err) => console.log("MongoDB Not Connected"))
+    .catch((err) => console.log("MongoDB Not Connected:", err.message));
 
-//start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
-import Button from './components/Button';
+//import Button from './components/Button';
 import Footer from './components/Footer';
 import Home from "./pages/static/Home";
-import Navbar from "./components/Navbar";
+//import Navbar from "./components/Navbar";
+import InstructorNavbar from "./components/instructorNavbar";
 import WhoWeAre from "./pages/static/WhoWeAre";
 import Programmes from "./pages/static/Programmes";
 import News from "./pages/static/News";
 import ContactUs from "./pages/static/ContactUs";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import AddAnnouncement from './components/AddAnnouncement';
+import AddedAnnouncement from './components/AddedAnnouncement';
+import UpdateAnnouncement from './components/UpdateAnnouncement';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <InstructorNavbar/>
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/whoweare" element={<WhoWeAre />} />
         <Route path="/programmes" element={<Programmes />} />
         <Route path="/news" element={<News />} />
         <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/addannouncement" element={<AddAnnouncement />} />
+        <Route path="/addedannouncement" element={<AddedAnnouncement />} />
+        <Route path="/updateannouncement/:id" element={<UpdateAnnouncement />} />
       </Routes>
-      <Button/>
-    
       <Footer />
     </div>
   );
