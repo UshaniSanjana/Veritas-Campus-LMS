5b519cd4bd3178dc10a273771e271a212b1836f7
Ushani-Sanjana <ush.sanjana@gmail.com>
2025-06-03 13:37:59 +0530

    Add resolved files and .gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
+
 //import Button from './components/Button';
-=======
->>>>>>> 24f76d267dfbb8ff8e5749aa2d60bf311f7b5f93
-import Footer from './components/Footer';
+
+import Footer from "./components/Footer";
 import Home from "./pages/static/Home";
 //import Navbar from "./components/Navbar";
 import InstructorNavbar from "./components/instructorNavbar";
@@ -11,46 +10,42 @@ import WhoWeAre from "./pages/static/WhoWeAre";
 import Programmes from "./pages/static/Programmes";
 import News from "./pages/static/News";
 import ContactUs from "./pages/static/ContactUs";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-<<<<<<< HEAD
-import AddAnnouncement from './components/AddAnnouncement';
-import AddedAnnouncement from './components/AddedAnnouncement';
-import UpdateAnnouncement from './components/UpdateAnnouncement';
-=======
+
+import AddAnnouncement from "./components/AddAnnouncement";
+import AddedAnnouncement from "./components/AddedAnnouncement";
+import UpdateAnnouncement from "./components/UpdateAnnouncement";
+
 import AddQuiz from "./pages/instructor/AddQuiz";
 import QuizList from "./pages/instructor/QuizList";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import UpdateQuiz from "./pages/instructor/UpdateQuiz";
-import QuizPerformance from './pages/instructor/QuizPerformance';
-
->>>>>>> 24f76d267dfbb8ff8e5749aa2d60bf311f7b5f93
+import QuizPerformance from "./pages/instructor/QuizPerformance";
 
 function App() {
   return (
     <div>
-      <InstructorNavbar/>
-    <Routes>
+      <InstructorNavbar />
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/whoweare" element={<WhoWeAre />} />
         <Route path="/programmes" element={<Programmes />} />
         <Route path="/news" element={<News />} />
         <Route path="/contactus" element={<ContactUs />} />
-<<<<<<< HEAD
+
         <Route path="/addannouncement" element={<AddAnnouncement />} />
         <Route path="/addedannouncement" element={<AddedAnnouncement />} />
-        <Route path="/updateannouncement/:id" element={<UpdateAnnouncement />} />
-      </Routes>
-=======
+        <Route
+          path="/updateannouncement/:id"
+          element={<UpdateAnnouncement />}
+        />
         <Route path="/addquiz" element={<AddQuiz />} />
         <Route path="/quizlist" element={<QuizList />} />
         <Route path="/updatequiz/:id" element={<UpdateQuiz />} />
         <Route path="/performance/:id" element={<QuizPerformance />} />
-
       </Routes>
-      
-    
->>>>>>> 24f76d267dfbb8ff8e5749aa2d60bf311f7b5f93
+
       <Footer />
     </div>
   );
