880aa8b08a365e9a7c955f4a9f9d75f05742288c
Ushani-Sanjana <ush.sanjana@gmail.com>
2025-06-19 15:28:42 +0530

    Save server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const supportRoutes = require("./routes/Student/supportRoute");
 
 const quizRoutes = require("./routes/quizRoutes");
 
-const studentSupportRoutes = require("./routes/student/supportRoute");
 const quizAnswersRoute = require("./routes/Student/quizAnswersRoute");
 const studentRoutes = require("./routes/student");
 const instructorRoutes = require("./routes/instructorRoutes");
@@ -25,20 +24,12 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/student/support", supportRoutes);
-app.use("/api/auth", authRoutes);
-
-// Database Connection
-dotenv.config();
 
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-app.use(cors());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // to serve uploaded images
 
@@ -49,8 +40,10 @@ mongoose
   .catch((err) => console.log("MongoDB Not Connected", err));
 
 // Routes
-app.use("/api/student/support", studentSupportRoutes);
+
 app.use("/api/quizzes", quizRoutes);
 app.use("/api/quizAnswers", quizAnswersRoute);
 app.use("/api", studentRoutes);
 app.use("/api/instructor", instructorRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/student/support", supportRoutes);
