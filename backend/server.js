285e3ea4e70655d61699bbda6855d6faad82e5c7
tharu-nisansala <119388618+tharu-nisansala@users.noreply.github.com>
2025-06-27 07:40:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,25 +5,34 @@ const path = require("path");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/Student/authRoutes");
 const supportRoutes = require("./routes/Student/supportRoute");
+const adminSupportRoutes = require('./routes/adminsupportroutes');
+const adminReportRoutes = require('./routes/adminReportRoute');
+const courseStatsRoutes = require('./routes/adminCourseStats');
+const adminDashboardRoutes = require('./routes/adminDashboard');
 
-const quizRoutes = require("./routes/quizRoutes");
-
-const quizAnswersRoute = require("./routes/Student/quizAnswersRoute");
-const studentRoutes = require("./routes/student");
-const instructorRoutes = require("./routes/instructorRoutes");
-
-// Load env variables
-dotenv.config();
-
-// App
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/student/support', supportRoutes);
+app.use('/api/adminsupport', adminSupportRoutes);
+app.use('/api/adminReport', adminReportRoutes);
+app.use('/api/adminCourseStats', courseStatsRoutes);
+app.use('/api/adminDashboard', adminDashboardRoutes);
+
+
+
+// Database Connection
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Connection Error:", err));
 
 // Start Server
 const PORT = process.env.PORT || 5000;
@@ -31,19 +40,3 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // to serve uploaded images
-
-// Database Connection
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("MongoDB Not Connected", err));
-
-// Routes
-
-app.use("/api/quizzes", quizRoutes);
-app.use("/api/quizAnswers", quizAnswersRoute);
-app.use("/api", studentRoutes);
-app.use("/api/instructor", instructorRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/student/support", supportRoutes);
