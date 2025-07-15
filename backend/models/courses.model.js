01947b4fd972e7586eaf34858e168c63cef2e9d7
Ushani-Sanjana <ush.sanjana@gmail.com>
2025-07-15 11:28:04 +0530

    Resolve errors in student model import

--- backend/models/courses.model.js
+++ backend/models/courses.model.js
@@ -1,22 +1,25 @@
 // models/course.model.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const courseSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    // instructor: [String], // Optional
+    modules: [{ type: mongoose.Schema.Types.ObjectId, ref: "Module" }],
   },
-  description: {
-    type: String,
-    trim: true,
-  },
-  // instructor: [String], // Optional
-  modules: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Module' }] 
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
 module.exports = Course;
--- backend/routes/adminDashboard.js
+++ backend/routes/adminDashboard.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const Enrollment = require("../models/enrollment.model");
-const Student = require("../models/student.model");
+const Student = require("../models/Student/student.model");
 const Instructor = require("../models/Instructors");
 const PasswordChange = require("../models/UserChanges");
-const Course = require("../models/Courses");
+const Course = require("../models/courses.model");
 const Assignment = require("../models/assignmentmodel");
 const AssignmentSubmission = require("../models/AssignmentSubmission");
 const Exam = require("../models/Exam");
@@ -14,7 +14,7 @@ const Material = require("../models/Material");
 const Quiz = require("../models/Quiz");
 const StudentSupport = require("../models/Student/supportModel");
 const Lecture = require("../models/Lecture");
-const QuizeSubmition = require("../models/QuizAnswer.model");
+const QuizeSubmition = require("../models/Student/QuizAnswer.model");
 
 // Existing dashboard route
 router.get("/dash", async (req, res) => {
